f068559219951f5d3be82eae2cf124cd98a4b1f5
kk <kk@tinymobilerobots.com>
2019-09-12 13:18:43 +0200

    Remove auto app start on apk update

--- android/MyPackageReplacedEventReceiver.java
+++ android/MyPackageReplacedEventReceiver.java
@@ -7,11 +7,11 @@ import android.content.Intent;
 /**
  * Starts {@link HomeActivity} after the app APK is updated.
  */
-public class MyPackageReplacedEventReceiver extends BroadcastReceiver {
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        Intent newIntent = new Intent(context, HomeActivity.class);
-        newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(newIntent);
-    }
-}
+// public class MyPackageReplacedEventReceiver extends BroadcastReceiver {
+//     @Override
+//     public void onReceive(Context context, Intent intent) {
+//         Intent newIntent = new Intent(context, HomeActivity.class);
+//         newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//         context.startActivity(newIntent);
+//     }
+// }
